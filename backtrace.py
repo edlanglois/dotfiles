f32e0835b611a2e57f6eed18e1e7152dbcf7bde1
Eric Langlois <contact@eric-langlois.ca>
2015-06-27 15:34:43 -0400

    backtrace.py: Prints the backtrace of a crashing program
    Requires GDB and the termcolor python package.

---
+++ backtrace.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Run a program and print the colorized backtrace.
+import argparse
+import locale
+import re
+import subprocess
+import sys
+from termcolor import colored
+
+def ColourBacktraceLine(line):
+    match = re.search(r'(.*?at\s*)(\S+)(\s*:\s*)(\d+)(.*)', line, flags=re.DOTALL)
+    if (match):
+        prefix, filename, colon, lineno, suffix = match.groups()
+        return ''.join((prefix, colored(filename, 'green'), colon,
+                        colored(lineno, 'yellow'), suffix))
+    else:
+        return line
+
+def PrettyPrintBacktrace(stream):
+    return ''.join(map(ColourBacktraceLine, stream))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Pretty-print the stack trace of a crashing program. "
+                    "Creates a backtrace with GDB then colourizes it.")
+    parser.add_argument('program', help='Program to run.', type=str)
+    args = parser.parse_args()
+
+    gdbArgs = ['gdb', '-batch', '-ex', 'run', '-ex', 'bt', args.program]
+    with subprocess.Popen(
+            gdbArgs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+            universal_newlines=True) as p:
+        sys.stdout.write(PrettyPrintBacktrace(p.stdout))
