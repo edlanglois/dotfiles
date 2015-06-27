365c431d17d4bba4a5079e5ea60c4b7d3fea5ca1
Eric Langlois <contact@eric-langlois.ca>
2015-06-27 19:44:38 -0400

    fish: Add /home/eric/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,6 @@ set -x LESS_TERMCAP_se (printf "\e[0m")
 set -x LESS_TERMCAP_so (printf "\e[38;5;246m")
 set -x LESS_TERMCAP_ue (printf "\e[0m")
 set -x LESS_TERMCAP_us (printf "\e[04;38;5;146m")
+
+# Add user's bin to path
+set --global fish_user_paths $fish_user_paths $HOME/bin
