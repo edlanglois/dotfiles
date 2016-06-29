ad3f72e90dd1e955551550c3e14cb1ee19579209
Eric Langlois <eric@langlois.xyz>
2016-06-28 21:16:46 -0700

    persistent-configs: Fix Nautilus on i3

--- set-persistent-configs.sh
+++ set-persistent-configs.sh
@@ -6,6 +6,11 @@ set -eux
 if hash gsettings 2>/dev/null && [ -n "${DISPLAY:-}" ]; then
 	# Set Capslock to the control key on GNOME
 	gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+	# Fix Nautilus on i3.
+	# Otherwise creates an invisible, unresponsive something when opened.
+	gsettings set org.gnome.desktop.background show-desktop-icons false
+
 fi
 
 # Generate the font configuration - Fonts must have been installed first.
