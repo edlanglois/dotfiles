e0e874f170bf1f5df447133d4d0459c5c1048e0a
Eric Langlois <eric@vectorinstitute.ai>
2018-04-20 18:37:36 -0400

    install-noto-fonts installs NotoSansMono as well

--- utils/install-noto-fonts.sh
+++ utils/install-noto-fonts.sh
@@ -3,9 +3,12 @@
 set -eux
 
 cd ~/Downloads
-wget --no-clobber https://noto-website.storage.googleapis.com/pkgs/Noto-hinted.zip
+wget --no-clobber "https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip"
+wget --no-clobber "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMono-hinted.zip"
 unzip Noto-hinted.zip -d noto
+unzip NotoSansMono-hinted.zip -d noto
 cp noto/*otf ~/.fonts
+cp noto/*ttf ~/.fonts
 
 # Generate the font configuration - Fonts must have been installed first.
 if hash fc-cache 2>/dev/null; then
