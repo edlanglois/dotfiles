c56ebd46ea51c1c2aec27ad560328ab7f3808889
Eric Langlois <eric@langlois.xyz>
2019-11-24 01:24:37 -0500

    set-persistent-configs: Don't set default-web-browser if $BROWSER is set

--- utils/set-persistent-configs.sh
+++ utils/set-persistent-configs.sh
@@ -20,7 +20,7 @@ fi
 
 # Program Defaults
 DEFAULT_BROWSER=$(./env/browser | grep 'BROWSER=' | sed 's/.*=//')
-if [ -n "$DEFAULT_BROWSER" ]; then
+if [ -n "$DEFAULT_BROWSER" -a -z "$BROWSER" ]; then
 	xdg-settings set default-web-browser ${DEFAULT_BROWSER}.desktop
 fi
 
