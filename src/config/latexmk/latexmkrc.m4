edlanglois/dotfiles

commit 6695fcaf715b39b3c1c00886d3127c0889d745be

latexmk: biber: Warn on validation errors instead of stopping
Validation is overly strict. It's not unreasonable for supposedly 'required'
fields to be missing. The formatting deals with it fine.

src/config/latexmk/latexmkrc.m4
@@ -1,6 +1,6 @@
 $pdf_mode = 1;
 $pdflatex = "pdflatex -interaction=nonstopmode %O %S";
-$biber = "biber --validate-datamodel --dieondatamodel %O %S";
+$biber = "biber --validate-datamodel
 # Force deletion of .bbl even if no .bib exists
 $bibtex_use = 2;
 # Created by biber; not cleaned by default (don't know why)

commit 7af1c07dc743ff0fac6dab988eb76eda0aecf0c5

latexmk: Fix syntax error introduced by 6695fca

src/config/latexmk/latexmkrc.m4
@@ -1,6 +1,6 @@
 $pdf_mode = 1;
 $pdflatex = "pdflatex -interaction=nonstopmode %O %S";
-$biber = "biber --validate-datamodel
+$biber = "biber --validate-datamodel";
 # Force deletion of .bbl even if no .bib exists
 $bibtex_use = 2;
 # Created by biber; not cleaned by default (don't know why)